26ddaa6addce5e16a5cc4e425556b0868b7ac6bf
Nandini Singh <nandinisingh2281@gmail.com>
2026-05-22 01:55:31 +0530

    fixed contact route

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
   };
 
   try {
-    const response = await fetch('https://portfolio-backend-wzon.onrender.com', {
+    const response = await fetch('https://portfolio-backend-wzon.onrender.com/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
