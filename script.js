2674666b9848c3bd2d3e5ea26d9755790595922f
Nandini Singh <nandinisingh2281@gmail.com>
2026-05-22 01:51:33 +0530

    fixed linkd and backend

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
   };
 
   try {
-    const response = await fetch('http://localhost:5000/contact', {
+    const response = await fetch('https://portfolio-backend-wzon.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
