08cf01a560b4c84247272ebfb213e292afc3521e
Nandini Singh <nandinisingh2281@gmail.com>
2026-05-22 01:22:15 +0530

    updated portfolio design

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ form.addEventListener('submit', async (e) => {
     responseMsg.innerText = 'Something broke bestie 💀';
   }
 });
+const typingText = document.querySelector('.typing');
+
+const words = [
+  'Full Stack Developer',
+  'Creative Coder',
+  'UI Enthusiast',
+  'JavaScript Developer'
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+
+  const currentWord = words[wordIndex];
+
+  if (isDeleting) {
+    typingText.textContent = currentWord.substring(0, charIndex--);
+  } else {
+    typingText.textContent = currentWord.substring(0, charIndex++);
+  }
+
+  let speed = isDeleting ? 50 : 100;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+
+    speed = 1500;
+    isDeleting = true;
+
+  } else if (isDeleting && charIndex === 0) {
+
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+
+  }
+
+  setTimeout(typeEffect, speed);
+}
+
+typeEffect();
